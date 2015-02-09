02fb420bc6dd1d9e4e90261d57031c4467a02830
liuxianming <liuxianming@gmail.com>
2015-02-09 13:01:33 -0600

    initial commit from avocado

---
+++ emacs/.emacs.d/elpa/auto-complete-c-headers-20140325.835/auto-complete-c-headers-autoloads.el
@@ -0,0 +1,18 @@
+;;; auto-complete-c-headers-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("auto-complete-c-headers-pkg.el" "auto-complete-c-headers.el")
+;;;;;;  (21394 49177 863940 0))
+
+;;;***
+
+(provide 'auto-complete-c-headers-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-complete-c-headers-autoloads.el ends here
