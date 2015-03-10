aa89bc4019d0c6024846bcab70e44aa4c04eb2ee
liuxianming <liuxianming@gmail.com>
2015-03-10 03:05:32 -0500

    mac

--- conf_emacs.sh
+++ conf_emacs.sh
@@ -3,4 +3,12 @@ git clone https://github.com/redguardtoo/emacs.d.git .emacs.d
 
 emacs -nw --batch -l ~/.emacs.d/init.el -f package-refresh-contents
 
-sudo apt-get install w3m lua aspell sbcl tidy xsel clang pyflakes
+if [ "$(uname)" == "Darwin" ]; then
+    # configuration for mac, not linux
+    brew install w3m lua aspell emacs-clang-complete-async
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt-get install w3m lua aspell sbcl tidy xsel clang
+fi
+
+#install percol
+sudo pip install percol pyflakes
