ddb707bc41dcdc0cbe8e4308ffea51a9c0c5d9f5
liuxianming <liuxianming@gmail.com>
2015-04-01 23:54:46 -0500

    I don't know what happend

--- emacs/emacs.d/lisp/init-themes.el
+++ emacs/emacs.d/lisp/init-themes.el
@@ -7,6 +7,7 @@
 ;;------------------------------------------------------------------------------
 ;; Old-style color theming support (via color-theme.el)
 ;;------------------------------------------------------------------------------
+;(defcustom window-system-color-theme 'color-theme-sanityinc-tomorrow-dark
 (defcustom window-system-color-theme 'color-theme-sanityinc-solarized-dark
   "Color theme to use in window-system frames.
 If Emacs' native theme support is available, this setting is
