ecd2e8c3d35daa1dd989a1c4e87b24187045e375
Xianming Liu <liuxianming@gmail.com>
2015-06-18 17:31:15 -0700

    Support Mac jedi

--- emacs/emacs.d/lisp/init-exec-path.el
+++ emacs/emacs.d/lisp/init-exec-path.el
@@ -4,8 +4,14 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
-
+;; To support jedi on Mac OS
+;; pip uninstall epc
+;; pip uninstall jedi
+;; pip install --upgrade pip
+;; pip install epc
+;; pip install jedi
 (when (memq window-system '(mac ns))
+  (exec-path-from-shell-copy-env "PYTHONPATH")
   (exec-path-from-shell-initialize))
 
 (provide 'init-exec-path)
