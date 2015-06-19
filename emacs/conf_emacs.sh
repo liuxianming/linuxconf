b1e3009188c19841c42b63cf43171f1213dddbdd
Xianming Liu <liuxianming@gmail.com>
2015-06-18 17:32:36 -0700

    Support jedi on Mac OS

--- emacs/conf_emacs.sh
+++ emacs/conf_emacs.sh
@@ -7,6 +7,11 @@ emacs -nw --batch -l ~/.emacs.d/init.el -f package-refresh-contents
 if [ "$(uname)" == "Darwin" ]; then
     # configuration for mac, not linux
     brew install w3m lua aspell emacs-clang-complete-async
+    sudo pip uninstall epc
+    sudo pip uninstall jedi
+    sudo pip install --upgrade pip
+    sudo pip install epc
+    sudo pip install jedi
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo apt-get install w3m lua aspell sbcl tidy xsel clang
 fi
