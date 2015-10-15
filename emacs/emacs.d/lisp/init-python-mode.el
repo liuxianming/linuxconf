1d76e2dd3a3d0a7e5685bf3a0f75cbeb5862d96e
Xianming Liu <liuxianming@gmail.com>
2015-10-15 13:56:53 -0700

    Update init-python-mode.el

--- emacs/emacs.d/lisp/init-python-mode.el
+++ emacs/emacs.d/lisp/init-python-mode.el
@@ -1,7 +1,11 @@
 ;;; package init-python-mode Summary
 ;; Configuration of Python IDE
-(require-package 'highlight-indentation)
-(require 'highlight-indentation)
-(load-file "~/.emacs.d/site-lisp/emacs-for-python/epy-init.el")
+(require-package 'elpy)
+(elpy-enable)
+;; define yas-expand
+(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
+;; define iedit mode
+(define-key global-map (kbd "C-c o") 'iedit-mode)
+
 (provide 'init-python-mode)
 ;;; init-python-mode ends here
