6c3a7b69c073ecbc2b830936c64ca294475baf50
liuxianming <liuxianming@gmail.com>
2015-10-24 19:13:55 -0500

    Refactoring all setting ups

--- install.sh
+++ install.sh
@@ -1,6 +1,38 @@
-# Installing Zsh + YADR/prezto
+if [ "$(uname)" == "Darwin" ]; then
+  # install homebrew
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+  cmd=brew
+  # python on mac need to be reinstalled
+  brew install python
+else:
+  cmd=sudo apt-get
+  sudo apt-get update
+  sudo apt-get install python-pip
+fi
 
-# for linux:
+# first set up git and download config
+${cmd} install git
+git config --global user.name "liuxianming"
+git config --global user.email "liuxianming@gmail.com"
+git clone git@github.com:liuxianming/linuxconf.git $HOME/unixconf
+cd $HOME/unixconf
+
+# setup python
+sudo pip install -r python_package_list.txt
+# setup all packages using apt-get / brew
+${cmd} install < ./sys_package_list.txt
+
+# protobuf
+git clone git@github.com:google/protobuf.git protobuf
+cd $HOME/protobuf
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+
+# Installing Zsh + YADR/prezto
 if [ "$(uname)" == "Darwin" ]; then
   sh -c "`curl -fsSL https://raw.github.com/skwp/dotfiles/master/install.sh`"
 else
@@ -12,27 +44,15 @@ else
   rbenv shell 2.0.0-p353
   sh -c "`curl -fsSL https://raw.github.com/skwp/dotfiles/master/install.sh`"
 fi
-
 # copy the tmux configuration
 cp ./tmux.conf ~/.tmux.conf
 
 # Configure emacs
 cp -rf ./emacs/emacs.d ~/.emacs.d
-sudo pip install --upgrade rope
-sudo pip install --upgrade epc jedi
-sudo pip install --upgrade flake8
-sudo pip install --upgrade importmagic
-sudo pip install --upgrade percol pyflakes autopep8
-sudo pip install --upgrade virtualenv
-
 # color theme
 git clone https://github.com/sellout/emacs-color-theme-solarized.git ~/.emacs.d/site-lisp/emacs-color-theme-solarized
-
 # setting up cpplint.py
 wget http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py
 sudo mv cpplint.py /usr/local/bin/cpplint.py
 sudo chmod 755 /usr/local/bin/cpplint.py
-
-# run emacs to complete configuration
-# git clone https://github.com/gabrielelanaro/emacs-for-python.git .emacs.d/site-lisp/emacs-for-python
 emacs -nw
