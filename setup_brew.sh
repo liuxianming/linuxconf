d00469dc218737ebf62f63b8e145b032a65ba95b
Xianming Liu <liuxianming@gmail.com>
2015-12-18 17:25:43 -0800

    Update setup_brew.sh

--- setup_brew.sh
+++ setup_brew.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+brew install brew-cask
+
+# setup ruby
+brew install rbenv
+brew install rbenv-gem-rehash
+brew install ruby-build
+rbenv install 2.1.2
+rbenv global 2.1.2
+gem update --system
+gem install bundler jekyll rails
+# install node
+brew install node
+
 brew install -vd snappy leveldb gflags glog szip lmdb
 brew tap homebrew/science
 brew install hdf5 opencv
@@ -8,3 +21,18 @@ brew install --build-from-source -vd boost boost-python
 brew install protobuf boost
 brew install emacs openssl cmake
 brew install mosh
+brew install macvim --override-system-vim
+# install other softwares
+echo Install Core Apps
+brew install x2goclient
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" java
+brew cask install --appdir="/Applications" google-chrome
+brew cask --appdir="/Applications" install sublime-text3
+
+# cleanup
+echo ClearnUp of Homebrew
+brew cleanup --force
+rm -f -r /Library/Caches/Homebrew/*
