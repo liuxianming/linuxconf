b3c686bcbf8e1f9cb60e4b5f24fc5b83e3cb83b3
Xianming Liu <liuxianming@gmail.com>
2016-01-05 23:42:42 -0600

    Change c++ configuration: using company mode and helm

--- emacs/emacs.d/lisp/init-ccpp.el
+++ emacs/emacs.d/lisp/init-ccpp.el
@@ -3,41 +3,13 @@
 
 ;; auto-compelete
 ; start auto-complete with emacs
-(require-package 'auto-complete)
 (require-package 'yasnippet)
 (require-package 'cuda-mode)
 (require-package 'google-c-style)
-(require-package 'auto-complete-c-headers)
-
-(require 'auto-complete)
-; do default config for auto-complete
-(require 'auto-complete-config)
-(ac-config-default)
 
 ;; Yasnippet
 (require 'yasnippet)
 (yas-global-mode 1)
-; let's define a function which initializes auto-complete-c-headers and gets called for c/c++ hooks
-(defun my:ac-c-header-init ()
-  (require 'auto-complete-c-headers)
-  (add-to-list 'ac-sources 'ac-source-c-headers)
-  ;; for mac platform, using
-  ;; gcc -xc++ -E -v -
-  ;; to find the xcode include folder
-  (add-to-list 'achead:include-directories '"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include")
-  ; for linux workstation, use:
-  (add-to-list 'achead:include-directories '"/opt/intel/composer_xe_2015.0.090/ipp/include")
-  (add-to-list 'achead:include-directories '"/opt/intel/composer_xe_2015.0.090/mkl/include")
-  (add-to-list 'achead:include-directories '"/opt/intel/composer_xe_2015.0.090/tbb/include")
-  (add-to-list 'achead:include-directories '"/usr/include/c++/4.8")
-  (add-to-list 'achead:include-directories '"/usr/include/x86_64-linux-gnu/c++/4.8")
-  (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-linux-gnu/4.8/include")
-  (add-to-list 'achead:include-directories '"/usr/local/include")
-  (add-to-list 'achead:include-directories '"/usr/include")
-)
-;; now let's call this function from c/c++ hooks
-(add-hook 'c++-mode-hook 'my:ac-c-header-init)
-(add-hook 'c-mode-hook 'my:ac-c-header-init)
 
 ;; Fix iedit bug in Mac
 (define-key global-map (kbd "C-c ;") 'iedit-mode)
@@ -91,4 +63,123 @@
 
 (global-semantic-idle-scheduler-mode 1)
 
+;;----------------------------------------------------------------------------
+;; settins from tuhdo on github
+(require-package 'anzu)
+(require-package 'company)
+(require-package 'duplicate-thing)
+(require-package 'ggtags)
+(require-package 'helm)
+(require-package 'helm-gtags)
+(require-package 'helm-projectile)
+(require-package 'helm-swoop)
+;; function-args
+(require-package 'clean-aindent-mode)
+(require-package 'comment-dwim-2)
+(require-package 'dtrt-indent)
+(require-package 'ws-butler)
+(require-package 'iedit)
+(require-package 'smartparens)
+(require-package 'projectile)
+(require-package 'volatile-highlights)
+(require-package 'zygospore)
+(require 'setup-helm)
+(require 'setup-helm-gtags)
+;; (require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
+
+(windmove-default-keybindings)
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+
+;; company
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(delete 'company-semantic company-backends)
+(define-key c-mode-map  [(tab)] 'company-complete)
+(define-key c++-mode-map  [(tab)] 'company-complete)
+;; (define-key c-mode-map  [(control tab)] 'company-complete)
+;; (define-key c++-mode-map  [(control tab)] 'company-complete)
+
+;; company-c-headers
+(add-to-list 'company-backends 'company-c-headers)
+
+;; hs-minor-mode for folding source code
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+
+(global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
+
+;; activate whitespace-mode to view all whitespace characters
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+;; show unncessary whitespace that can mess up your diff
+(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+
+;; use space to indent by default
+(setq-default indent-tabs-mode nil)
+
+;; set appearance of a tab that is represented by 4 spaces
+(setq-default tab-width 4)
+
+;; Compilation
+(global-set-key (kbd "<f5>") (lambda ()
+                               (interactive)
+                               (setq-local compilation-read-command nil)
+                               (call-interactively 'compile)))
+
+;; setup GDB
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t
+ )
+
+;; Package: clean-aindent-mode
+(require 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+
+;; Package: dtrt-indent
+(require 'dtrt-indent)
+(dtrt-indent-mode 1)
+
+;; Package: ws-butler
+(require 'ws-butler)
+(add-hook 'prog-mode-hook 'ws-butler-mode)
+
+;; Package: yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Package: smartparens
+(require 'smartparens-config)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+(smartparens-global-mode 1)
+
+;; Package: projejctile
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+(require 'helm-projectile)
+(helm-projectile-on)
+(setq projectile-completion-system 'helm)
+(setq projectile-indexing-method 'alien)
+
+;; Package zygospore
+(global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+
+;;----------------------------------------------------------------------------
+;; end of package define
 (provide 'init-ccpp)
